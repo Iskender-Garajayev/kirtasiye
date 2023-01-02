644b98961b0ffceb8a04c3836a1820e9b7853c7e
Iskender-Garajayev <yoloten.isa@gmail.com>
2023-01-02 03:26:48 +0300

    update

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './home.css'
+import { FontAwesomeIcon,} from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 const Home = () => {
   return (
     <>
     {/* header section start */}
       <div className="header">
-        <div className="logo"></div>
+        <div className="logo">
+        <span>Kirtasiye <FontAwesomeIcon icon={faCartShopping} /></span>
+        </div>
         <div className="navbar">
           <ul>
             <li>Sparis ver</li>
