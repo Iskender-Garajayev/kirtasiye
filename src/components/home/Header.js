f7ebe281d3e06dafd3c1fb40e95040e429f293df
Iskender-Garajayev <yoloten.isa@gmail.com>
2023-03-11 23:38:13 +0300

    update

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,41 +1,41 @@
-import React from 'react'
-import './header.css'
-import { FontAwesomeIcon,} from '@fortawesome/react-fontawesome'
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import "./header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
     <>
-    {/* header section start */}
-      <div className="header">
-        <div className="logo">
-        <span>Kirtasiye <FontAwesomeIcon icon={faCartShopping} /></span>
-        </div>
-        <div className="navbar">
-          <ul>
-            <li>Sparis ver</li>
-            <li>Blog</li>
-            <li>Yorumlar</li>
-            <li>Iletisim</li>
-          </ul>
-        </div>
-        <div className="login">
-          <button className='header-btn'>Login</button>
+      {/* header section start */}
+      <div className="header-container">
+        <div className="header">
+          <div className="logo">
+            <span>
+              Kirtasiye <FontAwesomeIcon icon={faCartShopping} />
+            </span>
+          </div>
+          <div className="navbar">
+            <ul>
+              <li>Sparis ver</li>
+              <li>Blog</li>
+              <li>Yorumlar</li>
+              <li>Iletisim</li>
+            </ul>
+          </div>
+          <div className="login">
+            <button className="header-btn">Login</button>
+          </div>
         </div>
       </div>
-    {/* header section end */}
+      {/* header section end */}
 
+      {/* main section start */}
 
+      <div className="home">Home</div>
 
-    {/* main section start */}
-
-    <div className="home">
-      Home
-    </div>
-
-    {/* main section end */}
+      {/* main section end */}
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
